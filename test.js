178845226c04a0ba83fc37ed5196790b4f75cffb
sandeepkumar242007 <42542293+sandeepkumar242007@users.noreply.github.com>
2021-06-09 18:00:16 +0530

    added variables

--- test.js
+++ test.js
@@ -2,5 +2,12 @@ function myFunction(p1, p2) {
     return p1 * p2;   // The function returns the product of p1 and p2
   }
 function test() {
-  return true;
+
+  int a=10;
+  int b=20;
+  int c=a+b;
+  
+
+
+  return false;
 }
